cd8f4853b476005d2f4ef9601a6c6cc172b5641e
Jayson Wong <jaysonwongf@gmail.com>
2023-05-24 21:14:17 +0800

    update callbackurl

--- components/credentials/Signin.js
+++ components/credentials/Signin.js
@@ -124,7 +124,7 @@ export default function Signin() {
 
   // Google Handler function
   async function handleGoogleSignIn() {
-    signIn('google', { callbackUrl: 'https://touring.vercel.app' });
+    signIn('google', { callbackUrl: '/' });
     // setDisable(true);
   }
   // GitHub Handler function

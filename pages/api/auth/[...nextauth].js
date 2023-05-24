0c4ce675ad526f10f3b745e8ff1415282d58df83
Jayson Wong <jaysonwongf@gmail.com>
2023-05-24 22:19:07 +0800

    move provider logic nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -45,7 +45,44 @@ export default NextAuth({
       });
 
       // helper f(n)
-      authSignin(user, account, Users);
+      if (account?.provider === 'google' || account?.provider === 'github') {
+        const [fName, lName] = user.name.split(' ');
+    
+        const providerAuthData = {
+          id: user.id,
+          firstName: fName,
+          lastName: lName ?? '',
+          email: user.email,
+          image: user.image,
+          authProvider: account.provider,
+        };
+    
+        const exist = await Users.findOne({
+          email: user.email,
+        });
+    
+        if (exist) {
+          await Users.findOneAndUpdate(
+            { email: user.email },
+            {
+              id: user.id,
+              email: user.email,
+              image: user.image,
+              authProvider: account.provider,
+            },
+            { new: true }
+          );
+        } else {
+          Users.create({
+            id: providerAuthData.id,
+            firstName: providerAuthData.firstName,
+            lastName: providerAuthData.lastName,
+            email: providerAuthData.email,
+            image: providerAuthData.image,
+            authProvider: providerAuthData.authProvider,
+          });
+        }
+      }
 
       return true;
     },

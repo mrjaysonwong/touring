3d2c6f648bd5d78438796b23b487fa1ea603880d
Jayson Wong <jaysonwongf@gmail.com>
2023-05-25 13:38:54 +0800

    replace with session

--- pages/login.js
+++ pages/login.js
@@ -7,7 +7,7 @@ export default function Login() {
   const router = useRouter();
   const { data: session, status } = useSession();
 
-  if (status === 'authenticated') {
+  if (session) {
     router.replace('/');
   }
 

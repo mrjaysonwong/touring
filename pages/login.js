910211aecfb688bf989b7bd6349fdfd8224ee29e
Jayson Wong <jaysonwongf@gmail.com>
2023-05-25 13:23:53 +0800

    change useSession status

--- pages/login.js
+++ pages/login.js
@@ -6,7 +6,10 @@ import { useSession } from 'next-auth/react';
 export default function Login() {
   const router = useRouter();
   const { data: session, status } = useSession();
-  if (session) router.push('/');
+
+  if (status === 'authenticated') {
+    router.replace('/');
+  }
 
   return (
     <>

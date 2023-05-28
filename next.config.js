4e97398bb5d1aba8fbd6462a7624de83c7792924
Jayson Wong <jaysonwongf@gmail.com>
2023-05-28 20:37:12 +0800

    update env

--- next.config.js
+++ next.config.js
@@ -5,10 +5,6 @@ const nextConfig = {
   compiler: {
     emotion: true,
   },
-  env: {
-    NINJA_KEY: process.env.NINJA_KEY,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  },
   images: {
     domains: ['res.cloudinary.com', 'lh3.googleusercontent.com'],
   },
